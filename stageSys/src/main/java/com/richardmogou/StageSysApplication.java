82fb523ff2c0cb37a92f63ecab89e8700d2a95f0
richardmogou <richardmogou99@gmail.com>
2025-09-02 23:00:12 +0200

    configuration de l'application

---
+++ stageSys/src/main/java/com/richardmogou/StageSysApplication.java
@@ -0,0 +1,13 @@
+package com.richardmogou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StageSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StageSysApplication.class, args);
+	}
+
+}
