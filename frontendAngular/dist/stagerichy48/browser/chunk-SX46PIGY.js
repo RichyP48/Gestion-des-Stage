ec5eef16d7297e797dd7cfaef29ddd3e9b4e9c49
richardmogou <richardmogou99@gmail.com>
2025-09-09 13:26:05 +0200

    Deploy

---
+++ frontendAngular/dist/stagerichy48/browser/chunk-SX46PIGY.js
@@ -0,0 +1,1 @@
+var N=function(A){return A.STUDENT="STUDENT",A.COMPANY="COMPANY",A.FACULTY="FACULTY",A.ADMIN="ADMIN",A}(N||{});export{N as a};
---
+++ frontendAngular/dist/stagerichy48/browser/chunk-ZS4G4JJV.js
@@ -0,0 +1,1 @@
+var o={production:!1,apiUrl:"http://localhost:8080/api"};console.log("\u{1F527} Environment loaded:",{production:o.production,apiUrl:o.apiUrl,timestamp:new Date().toISOString()});export{o as a};
